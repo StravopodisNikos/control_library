5185944719bb559229a025f2b518b83a37f7736f
StravopodisNikos <stravnik.rob@gmail.com>
2019-04-17 12:24:00 +0300

    PD_computed torque_control - Works for minor joint position change - Inner loop is not yet constructed

--- PD_computed_torque_control_2DoF_MMD.m
+++ PD_computed_torque_control_2DoF_MMD.m
@@ -1,22 +1,28 @@
 function [doty] = PD_computed_torque_control_2DoF_MMD(M,C,G,torque_dist,Kpd,qd,dqd,ddqd,y)
-% Follows the procedure pesented in pa2.4.4 p.185 in Robot Manipulator
+% Follows the procedure pesented in p.188-189 in Robot Manipulator
 % Control - Theory and Practice 2nd ed.
-% doty = zeros(4,1);
-% qd = desired final position
+
+% To be implemented:
+% 1.Inner Loop for dynamic properties is not constructed yet, works for
+%   minor joint -position changes so that M,C,G ~= constant
+% 2.Kp,Kd parameters are manually controlled for critical damping
+% 3. torque should be calculated iteratively through dynamic equations
+% 4. disturbance function is not physical
+
+% qd = desired final joint position-velocity
 %  t = time
 %  y = [q dq] current q,dq
-
-%  e = qd - q  (1)
-% de = dqd- dq (2)
-% error_input = [e de];
-% dde = ddqd - ddq (3)
 % Kpd = [Kp | Kd]
 
-xd = [qd; dqd];
-% Define tracking error
-% error_input = xd - y; % input to controller
-error_input = y; % input to controller
 
+% Define desired final state
+xd = [qd; dqd]; % desired state always constant
+dot_xd = [dqd; ddqd];
+% Define tracking error
+% error_input = [e de];
+error_input = xd-y;
+%  e = qd - q  (1)
+% de = dqd- dq (2)
 e = error_input(1:2);
 de = error_input(3:4);
 
@@ -24,6 +30,7 @@ de = error_input(3:4);
 % M(q)*ddq + C(q,dq)*dq + G(q,dq) + torque_dist = torque (I)
 % eq.I is transformed to eq.II:
 % M(q)*ddq + N(q,dq) + torque_dist = torque (II)
+% M,N change for each configuration!!!
 N = C*y(3:4)+G; % 2x1
 
 %% PD control as in p.188
@@ -35,11 +42,11 @@ Kd = Kpd(1:2,3:4); % 2x2
 u = -Kp*e - Kd*de; % 2x1 % output from controller, input to robot
 
 % Torque applied from robot's actuators
-torque = M*(ddqd+u) + N
+torque = M*(ddqd-u) + N; % "should be avoided" p.188 Robot Manipulator Control
 
 % Disturbance function w(t) = M^-1*torque_dist
 % w = dde + Kd*de + Kp*e; % 2x1
-% w(1:2,:) = inv(M)*torque_dist;
+% w(1:2,:) = inv(M)*torque_dist; % inv is super high!!!
 w = [1 1]';
 
 % For state vector x = [e de] 4x1 vector
@@ -51,7 +58,7 @@ B = [O; I];
 C = eye(4);
 D = zeros(4,2);
 
-%% Create ss system
+%% Evaluate if ss system is controllable
 % % t=10;
 % % sys = ss(A,B,C,D,t); % PD_computed torque system to steady state
 % % ConMax = ctrb(sys.A,sys.B); % SS Controllability matrix
@@ -66,6 +73,7 @@ D = zeros(4,2);
 % % end
 
 %% System of differential equations to solve
-doty = A*y + B*w;
+dot_error_input = A*error_input + B*w;
+doty = dot_xd - dot_error_input;
 
 end
